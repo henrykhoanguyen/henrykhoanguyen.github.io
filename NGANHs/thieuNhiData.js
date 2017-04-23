20a3b643ad53afab36976c893697aa23207f5953
Khoa Nguyen <henrykhoanguyen@gmail.com>
2017-04-23 03:48:20 -0700

    events updated

--- NGANHs/thieuNhiData.js
+++ NGANHs/thieuNhiData.js
@@ -1,33 +1,37 @@
 var auInfo = {
-	title: ["Cute Dog"],
+	title: ["Đoàn VSL Pinic"],
 
-	time: ["12-2-2016"],
+	time: ["5/20/2017"],
 
 	description: [
 
-		"<p>Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their tiny stature, and even others for their massive size.</p><p>Many people also have their own barometers for what makes a cute dog.</p>"]
+		"<p><b>Time:</b> Saturday, May 20 – from 10am - 4pm <br><br><b>Location:</b> De Anza Park @ 5400-5598 Marquette Dr San Jose, CA 95118<br><br><b>Description:</b> Đoàn Vinh Sơn Liêm sẽ tổ chức cuộc dã ngoại ngoài trời nhằm xây dựng mối quan hệ bền vững giữa các trưởng và các em. Các em cũng sẽ có dịp được vui chơi giải trí với các trò chơi thể thao và đố vui đại học xoay quanh tình yêu và hình ảnh của Thiên Chúa.</p>"
+]
 };
 
 var thieuInfo = {
-	title: ["Cute Dog"],
+	title: ["Đoàn VSL Pinic"],
 
-	time: ["12-2-2016"],
+	time: ["5/20/2017"],
 
 	description: [
 
-		"<p>Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their tiny stature, and even others for their massive size.</p><p>Many people also have their own barometers for what makes a cute dog.</p>"
+		"<p><b>Time:</b> Saturday, May 20 – from 10am - 4pm <br><br><b>Location:</b> De Anza Park @ 5400-5598 Marquette Dr San Jose, CA 95118<br><br><b>Description:</b> Đoàn Vinh Sơn Liêm sẽ tổ chức cuộc dã ngoại ngoài trời nhằm xây dựng mối quan hệ bền vững giữa các trưởng và các em. Các em cũng sẽ có dịp được vui chơi giải trí với các trò chơi thể thao và đố vui đại học xoay quanh tình yêu và hình ảnh của Thiên Chúa.</p>"
 ]
 };
 
 var nghiaInfo = {
-	title: ["Cute Dog"
+	title: ["Đại Hội Nghĩa Hiệp",
+			"Đoàn VSL Pinic"
 			],
 
-	time: ["12-2-2016"],
+	time: ["5/27/2017 - 5/29/2017",
+			"5/20/2017"],
 
 	description: [
 
-		"<p>Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their tiny stature, and even others for their massive size.</p><p>Many people also have their own barometers for what makes a cute dog.</p>"
+		"<p><b>Time:</b> Saturday, May 27 (6pm) – Monday, May 29 (12pm) <br><br><b>Location:</b> Chau Son Monastery, Walnut Grove<br><br><b>Description:</b> Ngày hội ngộ và giao lưu cho các em Nghĩa và Hiệp Sỹ từ các đoàn bạn khác nhau.</p>",
+		"<p><b>Time:</b> Saturday, May 20 – from 10am - 4pm <br><br><b>Location:</b> De Anza Park @ 5400-5598 Marquette Dr San Jose, CA 95118<br><br><b>Description:</b> Đoàn Vinh Sơn Liêm sẽ tổ chức cuộc dã ngoại ngoài trời nhằm xây dựng mối quan hệ bền vững giữa các trưởng và các em. Các em cũng sẽ có dịp được vui chơi giải trí với các trò chơi thể thao và đố vui đại học xoay quanh tình yêu và hình ảnh của Thiên Chúa.</p>"
 ]
 };
 
